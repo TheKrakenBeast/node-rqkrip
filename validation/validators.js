c33d2f9cfddf1e167211942c2a87599c201ea1d8
TheKrakenBeast <t.oshea@vivaldi.net>
2023-01-17 14:39:12 +1100

    Step 2: Line number errors fixed.

--- validation/validators.js
+++ validation/validators.js
@@ -3,9 +3,9 @@ let lineError = require('../models/LineError');
 // ------------------ Main Validators -------------------- //
 
 async function validateEquipment(equipmentList, fieldMap, errors) {
-  validateColumns(equipmentList, fieldMap, errors);
-  validateData(equipmentList, fieldMap, errors);
-  validateUniqueKeys(equipmentList, fieldMap, errors);
+    validateColumns(equipmentList, fieldMap, errors);
+    validateData(equipmentList, fieldMap, errors);
+    validateUniqueKeys(equipmentList, fieldMap, errors);
 }
 
 // ------------------ Sub Validators -------------------- //
@@ -14,139 +14,148 @@ async function validateEquipment(equipmentList, fieldMap, errors) {
 
 function validateColumns(equipmentList, fieldMap, errors) {
 
-  let headers = Object.keys(equipmentList[0]);
+    let headers = Object.keys(equipmentList[0]);
 
-  for (col = 0; col < headers.length; col++) {
-    let field = fieldMap.get(headers[col]);
-    if (field) {
-      field.colIndex = col;
+    for (let col = 0; col < headers.length; col++) {
+        let field = fieldMap.get(headers[col]);
+        if (field) {
+            field.colIndex = col;
+        }
     }
-  }
 
-  // Check that All the required labels now exist in map
-  for (const item of fieldMap[Symbol.iterator]()) {
-    if (item[1].isRequired && item[1].colIndex == null) {
-      errors.push(new lineError('Required Column Missing', item[0], 'N/A'));
+    // Check that All the required labels now exist in map
+    for (const item of fieldMap[Symbol.iterator]()) {
+        if (item[1].isRequired && item[1].colIndex == null) {
+            errors.push(new lineError('Required Column Missing', item[0], 'N/A'));
+        }
     }
-  }
 }
 
 function validateData(equipmentList, fieldMap, errors) {
-  for (let equipment of equipmentList) {
-    let counter = 2;
+    // Row starts at 1 to produce human-readable line numbers
+    let row = 1;
+    for (let equipment of equipmentList) {
+        for (const item of fieldMap[Symbol.iterator]()) {
+            let column = item[0];
+            let vStr = item[1].validationString;
+            let attr = equipment[column];
 
-    for (const item of fieldMap[Symbol.iterator]()) {
-      let column = item[0];
-      let vStr = item[1].validationString;
-      let attr = equipment[column];
+            if (!vStr) {
+                continue;
+            }
 
-      if (!vStr) {
-        continue;
-      }
+            let reg = new RegExp(vStr);
 
-      let reg = new RegExp(vStr);
+            // If required, validate it.
+            // If the field is missing, don't validate it as we will already get an error
+            if (item[1].isRequired && item[1].colIndex != null) {
+                if (attr === '') {
+                    errors.push(
+                        new lineError(
+                            ` ${column} column cannot contain empty values`,
+                            attr,
+                            row
+                        )
+                    );
+                } else if (!reg.test(attr)) {
+                    errors.push(
+                        new lineError(
+                            `Invalid data for ${column}`,
+                            attr,
+                            row
+                        )
+                    );
 
-      // If required, validate it.
-      // If the field is missing, don't validate it as we will already get an error
-      if (item[1].isRequired && item[1].colIndex != null) {
-        if (attr == '') {
-          errors.push(
-            new lineError(
-              ` ${column} column cannot contain empty values`,
-              attr,
-              counter
-            )
-          );
-        } else if (!reg.test(attr)) {
-          errors.push(
-            new lineError(`Invalid data for ${column}`, attr, counter)
-          );
+                    // If not required, but not empty, validate it
+                } else if (!item[1].isRequired && attr !== '') {
+                    if (!reg.test(attr)) {
+                        errors.push(
+                            new lineError(
+                                `Invalid data for ${column}`,
+                                attr,
+                                row
+                            )
+                        );
+                    }
+                }
+            }
 
-          // If not required, but not empty, validate it
-        } else if (!item[1].isRequired && attr !== '') {
-          if (!reg.test(attr)) {
-            errors.push(
-              new lineError(`Invalid data for ${column}`, attr, counter)
-            );
-          }
+            // Validate column lengths
+            if (item[1].maxLength != null && attr !== '') {
+                if (attr.length > item[1].maxLength) {
+                    errors.push(
+                        new lineError(
+                            `Data too long for ${column}. Max ${item[1].maxLength} characters`,
+                            attr,
+                            row
+                        )
+                    );
+                }
+            }
         }
-      }
-
-      // Validate column lengths
-      if (item[1].maxLength != null && attr !== '') {
-        if (attr.length > item[1].maxLength) {
-          errors.push(
-            new lineError(
-              `Data too long for ${column}. Max ${item[1].maxLength} characters`,
-              attr,
-              counter
-            )
-          );
-        }
-      }
+        row++;
     }
-
-    counter++;
-  }
 }
 
 function validateUniqueKeys(equipmentList, fieldMap, errors) {
-  let uniqueKey = 1;
-  let cols = getKeyColums(fieldMap).get(uniqueKey);
-  let keyStrings = [];
+    // These are not required at this point
+    let uniqueKey = 1;
+    let cols = getKeyColumns(fieldMap).get(uniqueKey);
 
-  // Counter starts at 2 to compensate for excel line indexes
-  counter = 2;
+    let keyStrings = [];
 
-  // Push keys to array
-  for (let equipment of equipmentList) {
-    let update = equipment['Update To'] !== '' ? true : false;
-    let project = equipment['Project'];
-    let number = !update
-      ? equipment['Equipment Number']
-      : equipment['Update To'];
-    let key = project + number;
+    // Row starts at 1 to produce human-readable line numbers
+    let row = 1;
 
-    key = key.toLocaleLowerCase().replace(/\s/g, '');
-    keyStrings.push({ key, counter });
-  }
-  counter++;
+    // Push keys to array
+    for (let equipment of equipmentList) {
+        let update = equipment['Update To'] !== '';
+        let project = equipment['Project'];
+        let number = !update
+            ? equipment['Equipment Number']
+            : equipment['Update To'];
+        let key = project + number;
 
-  // Filter just duplicates
-  const duplicates = keyStrings.filter(
-    (key, index, self) => index !== self.findIndex((t) => t.key === key.key)
-  );
+        key = key.toLocaleLowerCase().replace(/\s/g, '');
+        keyStrings.push({key, row});
+        row++;
+    }
 
-  for (var key in duplicates) {
-    errors.push(
-      new lineError(
-        'Duplicate key value.',
-        duplicates[key].key,
-        duplicates[key].counter
-      )
+    // Filter just duplicates
+    const duplicates = keyStrings.filter(
+        (key, index, self) => index !== self.findIndex((t) => t.key === key.key)
     );
-  }
+
+    for (let key in duplicates) {
+        errors.push(
+            new lineError(
+                'Duplicate key value.',
+                duplicates[key].key,
+                duplicates[key].row,
+            )
+        );
+    }
 }
 
-function getKeyColums(fieldMap) {
-  let uniqueKeys = new Map();
+function getKeyColumns(fieldMap) {
+    let uniqueKeys = new Map();
 
-  for (const item of fieldMap[Symbol.iterator]()) {
-    let keyID = item[1].uniqueKeyID;
+    for (const item of fieldMap[Symbol.iterator]()) {
+        let keyID = item[1].uniqueKeyID;
 
-    if (!keyID) {
-      continue;
+        if (!keyID) {
+            continue;
+        }
+
+        if (uniqueKeys.has(keyID)) {
+            uniqueKeys.get(keyID).push(item[1].colIndex);
+        } else {
+            uniqueKeys.set(keyID, [item[1].colIndex]);
+        }
     }
-
-    if (uniqueKeys.has(keyID)) {
-      uniqueKeys.get(keyID).push(item[1].colIndex);
-    } else {
-      uniqueKeys.set(keyID, [item[1].colIndex]);
-    }
-  }
-  return uniqueKeys;
+    return uniqueKeys;
 }
 
 module.exports = {
-  validateEquipment,
+    validateEquipment,
 };
